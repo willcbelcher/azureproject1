089421413eed9c84f7b45205dc9ff70fab598787
William Belcher <willcbelcher@gmail.com>
2021-04-13 22:44:30 -0400

    trying to fix logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,7 +67,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.error("login unsuccessful")
+            app.logger.info("login unsuccessful")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
