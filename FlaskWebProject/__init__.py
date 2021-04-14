e936367bd95da917d0d20058254b065a3fd69422
William Belcher <willcbelcher@gmail.com>
2021-04-13 22:24:09 -0400

    trying to fix logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,7 +11,7 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-app.logger.setLevel(logging.INFO)
+# app.logger.setLevel(logging.INFO)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,14 +67,14 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.info("login unsuccessful")
+            app.logger.error("login unsuccessful")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-        app.logger.info("login successful")
+        app.logger.error("login successful")
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
