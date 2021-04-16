7ecb2341d3e6803d8c7b2b58ab26f7d5f107131d
William Belcher <willcbelcher@gmail.com>
2021-04-15 20:04:09 -0400

    fixing logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,11 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 app.logger.setLevel(logging.DEBUG)
+streamHandler = logging.streamHandler()
+streamHandler.setLevel(logging.DEBUG)
+app.logger.addHandler(streamHandler)
+
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
