9c7eaaf68afa220ba587d71b845cbf2eb3a1a787
William Belcher <willcbelcher@gmail.com>
2021-04-15 20:12:46 -0400

    fixing loggin

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,9 +10,9 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.logger.setLevel(logging.DEBUG)
-streamHandler = logging.streamHandler()
-streamHandler.setLevel(logging.DEBUG)
+app.logger.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
 
 
